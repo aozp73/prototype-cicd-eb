c8da5f53535d904d298cc19ecd69a635e368f502
aozp73 <aozp73@naver.com>
2023-09-05 12:37:27 +0900

    global 예외처리 클래스에 sentry 코드 적용

--- src/main/java/com/portfolio/portfolio_project/core/advice/MyExceptionAdvice.java
+++ src/main/java/com/portfolio/portfolio_project/core/advice/MyExceptionAdvice.java
@@ -12,36 +12,44 @@ import com.portfolio.portfolio_project.core.exception.Exception404;
 import com.portfolio.portfolio_project.core.exception.Exception500;
 import com.portfolio.portfolio_project.core.util.Script;
 
+import io.sentry.Sentry;
+
 @RestControllerAdvice
 public class MyExceptionAdvice {
 
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> badRequest(Exception400 e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> unAuthorized(Exception401 e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> forbidden(Exception403 e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception404.class)
     public ResponseEntity<?> notFound(Exception404 e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> serverError(Exception500 e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(CustomException.class) 
     public ResponseEntity<?> customException(CustomException e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(Script.back(e.getMessage()), e.getStatus());
     }
 }
