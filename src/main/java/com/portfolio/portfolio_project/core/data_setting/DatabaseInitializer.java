a95102b72c34197c7dc2656c0f7a0741cfd853dc
aozp73 <aozp73@naver.com>
2023-09-07 15:17:18 +0900

    BeanStalk - 환경변수 인식 에러 수정 6

--- src/main/java/com/portfolio/portfolio_project/core/data_setting/DatabaseInitializer.java
+++ src/main/java/com/portfolio/portfolio_project/core/data_setting/DatabaseInitializer.java
@@ -1,64 +1,64 @@
-package com.portfolio.portfolio_project.core.data_setting;
+// package com.portfolio.portfolio_project.core.data_setting;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
+// import java.time.LocalDateTime;
+// import java.util.Optional;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.stereotype.Component;
 
-import com.portfolio.portfolio_project.domain.jpa.myproject.enums.ProjectRole;
-import com.portfolio.portfolio_project.domain.jpa.myproject.my_project_role_code.MyProjectRoleCode;
-import com.portfolio.portfolio_project.domain.jpa.myproject.my_project_role_code.MyProjectRoleCodeRepository;
-import com.portfolio.portfolio_project.domain.jpa.skills.enums.SkillType;
-import com.portfolio.portfolio_project.domain.jpa.skills.my_skill_type_code.MySkillTypeCode;
-import com.portfolio.portfolio_project.domain.jpa.skills.my_skill_type_code.MySkillTypeCodeRepository;
-import com.portfolio.portfolio_project.domain.jpa.user.User;
-import com.portfolio.portfolio_project.domain.jpa.user.UserRepository;
+// import com.portfolio.portfolio_project.domain.jpa.myproject.enums.ProjectRole;
+// import com.portfolio.portfolio_project.domain.jpa.myproject.my_project_role_code.MyProjectRoleCode;
+// import com.portfolio.portfolio_project.domain.jpa.myproject.my_project_role_code.MyProjectRoleCodeRepository;
+// import com.portfolio.portfolio_project.domain.jpa.skills.enums.SkillType;
+// import com.portfolio.portfolio_project.domain.jpa.skills.my_skill_type_code.MySkillTypeCode;
+// import com.portfolio.portfolio_project.domain.jpa.skills.my_skill_type_code.MySkillTypeCodeRepository;
+// import com.portfolio.portfolio_project.domain.jpa.user.User;
+// import com.portfolio.portfolio_project.domain.jpa.user.UserRepository;
 
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
-@Component
-@RequiredArgsConstructor
-public class DatabaseInitializer implements CommandLineRunner {
+// @Component
+// @RequiredArgsConstructor
+// public class DatabaseInitializer implements CommandLineRunner {
 
-    private final UserRepository userRepository;
-    private final MyProjectRoleCodeRepository myProjectRoleCodeRepository;
-    private final MySkillTypeCodeRepository mySkillTypeCodeRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Override
-    public void run(String... args) {
+//     private final UserRepository userRepository;
+//     private final MyProjectRoleCodeRepository myProjectRoleCodeRepository;
+//     private final MySkillTypeCodeRepository mySkillTypeCodeRepository;
+//     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+//     @Override
+//     public void run(String... args) {
 
-        Optional<User> userCheck = userRepository.findByEmail("aozp73@naver.com");
+//         Optional<User> userCheck = userRepository.findByEmail("aozp73@naver.com");
 
-        if (!userCheck.isPresent()) {
-            String encPassword = bCryptPasswordEncoder.encode("1234");
-            User user = User.builder()
-                    .email("aozp73@naver.com")
-                    .password(encPassword)
-                    .role("admin")
-                    .createdAt(LocalDateTime.now())
-                    .build();
-            userRepository.save(user);
-        }
+//         if (!userCheck.isPresent()) {
+//             String encPassword = bCryptPasswordEncoder.encode("1234");
+//             User user = User.builder()
+//                     .email("aozp73@naver.com")
+//                     .password(encPassword)
+//                     .role("admin")
+//                     .createdAt(LocalDateTime.now())
+//                     .build();
+//             userRepository.save(user);
+//         }
 
-        for (ProjectRole role : ProjectRole.values()) {
-            MyProjectRoleCode myProjectRoleCode = MyProjectRoleCode.builder()
-                .projectRole(role)
-                .build();
-            Optional<MyProjectRoleCode> myProjectRoleCodeCheck = myProjectRoleCodeRepository.findByProjectRole(role);
-            if (!myProjectRoleCodeCheck.isPresent()) {
-                myProjectRoleCodeRepository.save(myProjectRoleCode);
-            }
-        }
-        for (SkillType type : SkillType.values()) {
-            MySkillTypeCode mySkillTypeCode = MySkillTypeCode.builder()
-                .skillType(type)
-                .build();
-            Optional<MySkillTypeCode> mySkillTypeCodeCheck = mySkillTypeCodeRepository.findBySkillType(type);
-            if (!mySkillTypeCodeCheck.isPresent()) {
-                mySkillTypeCodeRepository.save(mySkillTypeCode);
-            }
-        }
-    }   
-}
+//         for (ProjectRole role : ProjectRole.values()) {
+//             MyProjectRoleCode myProjectRoleCode = MyProjectRoleCode.builder()
+//                 .projectRole(role)
+//                 .build();
+//             Optional<MyProjectRoleCode> myProjectRoleCodeCheck = myProjectRoleCodeRepository.findByProjectRole(role);
+//             if (!myProjectRoleCodeCheck.isPresent()) {
+//                 myProjectRoleCodeRepository.save(myProjectRoleCode);
+//             }
+//         }
+//         for (SkillType type : SkillType.values()) {
+//             MySkillTypeCode mySkillTypeCode = MySkillTypeCode.builder()
+//                 .skillType(type)
+//                 .build();
+//             Optional<MySkillTypeCode> mySkillTypeCodeCheck = mySkillTypeCodeRepository.findBySkillType(type);
+//             if (!mySkillTypeCodeCheck.isPresent()) {
+//                 mySkillTypeCodeRepository.save(mySkillTypeCode);
+//             }
+//         }
+//     }   
+// }
